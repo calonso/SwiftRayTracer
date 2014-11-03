6428e78fad6e3ea2c0014ce8b425f36742f27ca3
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2014-11-03 19:10:19 +0000

    Initial Commit

---
+++ RayTracerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RayTracerExample
+//
+//  Created by Carlos Alonso on 03/11/2014.
+//  Copyright (c) 2014 MyDrive Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
